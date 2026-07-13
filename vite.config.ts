f11b503ab01022d4db88b12512fec613152e534a
BrightNighter <taming.tanaphat@gmail.com>
2026-07-13 15:41:25 +0700

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  base: "/mermaid-viewer/",
+  server: {
+    host: false,
+  },
+});
